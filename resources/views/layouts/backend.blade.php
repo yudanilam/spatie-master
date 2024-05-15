fb44ddb1b3e601bed9808b2a55c5cd3ba3330816
Syahru Rahmayuda <yudarahma@gmail.com>
2024-05-15 10:32:27 +0700

    Update backend.blade.php
    Merubah title

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title')</title>
+    <title>@yield('title')latihan</title>
 
     <!-- Google Font: Source Sans Pro -->
     {{-- @include('layouts.component.css') --}}
