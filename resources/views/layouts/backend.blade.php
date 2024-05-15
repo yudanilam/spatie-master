8593edcd3dcc62c1e7f46be7c9892706e0113281
Syahru Rahmayuda <yudarahma@gmail.com>
2024-05-15 10:34:15 +0700

    Update backend.blade.php
    Menghapus title latihan

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>@yield('title')latihan</title>
+    <title>@yield('title')</title>
 
     <!-- Google Font: Source Sans Pro -->
     {{-- @include('layouts.component.css') --}}
